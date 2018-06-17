21df2d84de8dd8733f085bd3f0cd546645734ee1
Aleksey Landyrev <landyrevag@gmail.com>
2018-06-17 22:04:40 +0300

    Amplitude implemented

--- app/src/main/java/ru/landyrev/howtodraw/util/Analytics.kt
+++ app/src/main/java/ru/landyrev/howtodraw/util/Analytics.kt
@@ -2,6 +2,7 @@ package ru.landyrev.howtodraw.util
 
 import android.app.Application
 import android.os.Bundle
+import com.amplitude.api.Amplitude
 import com.applovin.sdk.AppLovinSdk
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
@@ -9,6 +10,7 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import com.yandex.metrica.YandexMetrica
 import com.yandex.metrica.YandexMetricaConfig
 import io.fabric.sdk.android.Fabric
+import org.json.JSONObject
 import ru.landyrev.howtodraw.BuildConfig
 import ru.landyrev.howtodraw.data.LevelsData
 
@@ -24,6 +26,8 @@ object Analytics {
 
         val crashlyticsCore = CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()
         Fabric.with(context, Crashlytics.Builder().core(crashlyticsCore).build())
+
+        Amplitude.getInstance().initialize(app.applicationContext, "db2abc611179520932bdce78fce99602").enableForegroundTracking(app)
 
         YandexMetrica.activate(context, yandexMetricaConfig)
         YandexMetrica.enableActivityAutoTracking(app)
@@ -49,6 +53,7 @@ object Analytics {
         }
         YandexMetrica.reportEvent(eventName, params)
         logEvent(eventName, bundle)
+        Amplitude.getInstance().logEvent(eventName, JSONObject(params))
     }
 
     fun logEvent(eventName: String, params: Bundle) {
@@ -56,17 +61,16 @@ object Analytics {
     }
 
     fun collectUserProperties() {
-        mFirebaseAnalytics.setUserProperty(
-                "unlockedStages",
-                LevelsData.unlockedStagesCount.toString()
+        val userProperties = hashMapOf(
+                "unlockedStages" to LevelsData.unlockedStagesCount.toString(),
+                "totalStages" to LevelsData.stagesCount.toString(),
+                "isPro" to LevelsData.proMode.toString()
         )
-        mFirebaseAnalytics.setUserProperty(
-                "totalStages",
-                LevelsData.stagesCount.toString()
-        )
-        mFirebaseAnalytics.setUserProperty(
-                "isPro",
-                LevelsData.proMode.toString()
-        )
+        Amplitude.getInstance().clearUserProperties()
+        Amplitude.getInstance().setUserProperties(JSONObject(userProperties))
+        userProperties.forEach { t, u ->
+            mFirebaseAnalytics.setUserProperty(t, u)
+        }
+
     }
 }
